30a87fb37ac707f8958ccccde93f2a50b2056362
ahmethakanyldrm <65174957+ahmethakanyldrm@users.noreply.github.com>
2023-10-05 14:14:02 +0300

    Added Theme Constants

--- Tip Calculator/Tip Calculator/CalculatorViewController.swift
+++ Tip Calculator/Tip Calculator/CalculatorViewController.swift
@@ -5,68 +5,63 @@
 //  Created by AHMET HAKAN YILDIRIM on 22.06.2023.
 //
 
-import UIKit
 import SnapKit
+import UIKit
 
 class CalculatorViewController: UIViewController {
-    
     // MARK: - Properties
-    
+
     private let logoView = LogoView()
     private let resultView = ResultView()
     private let billInputView = BillInputView()
     private let tipInputView = TipInputView()
     private let splitInputView = SplitInputView()
-    
-    private lazy var vStackView : UIStackView = {
-        
-        let stackView = UIStackView(arrangedSubviews: [logoView, resultView, billInputView, tipInputView, splitInputView])
-        
+
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [logoView, resultView, billInputView, tipInputView, splitInputView, UIView()]
+        )
+
         stackView.axis = .vertical
         stackView.spacing = 36
         return stackView
     }()
-    
+
     // MARK: - LifeCycles
 
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
     }
-    
+
     private func layout() {
-        view.backgroundColor = .white
+        view.backgroundColor = ThemeColor.bg
         view.addSubview(vStackView)
-        
+
         vStackView.snp.makeConstraints { make in
             make.leading.equalTo(view.snp.leadingMargin).offset(16)
             make.trailing.equalTo(view.snp.trailingMargin).offset(-16)
             make.top.equalTo(view.snp.topMargin).offset(16)
             make.bottom.equalTo(view.snp.bottomMargin).offset(-16)
-            
         }
-        
+
         logoView.snp.makeConstraints { make in
             make.height.equalTo(48)
         }
-        
+
         resultView.snp.makeConstraints { make in
             make.height.equalTo(224)
         }
-        
+
         billInputView.snp.makeConstraints { make in
             make.height.equalTo(56)
         }
-        
+
         tipInputView.snp.makeConstraints { make in
             make.height.equalTo(56 + 56 + 16)
         }
-        
+
         splitInputView.snp.makeConstraints { make in
             make.height.equalTo(56)
         }
     }
-
-
 }
-
---
+++ Tip Calculator/Tip Calculator/Config/ThemeFont.swift
@@ -0,0 +1,26 @@
+//
+//  ThemeCont.swift
+//  Tip Calculator
+//
+//  Created by AHMET HAKAN YILDIRIM on 5.10.2023.
+//
+
+import UIKit
+
+struct ThemeFont {
+    // AvenirNext
+    static func regular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func bold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func demibold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    
+    
+}
